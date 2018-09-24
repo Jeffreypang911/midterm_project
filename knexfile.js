37d66d5fcdd15a880b0b46e8bb879cf6ed9cc3dd
bguids91 <bguids91@gmail.com>
2018-09-24 00:50:50 +0000

    Update kinex file

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: 'postgres: //vybyvcqprszotj:51720ea9b7ce6e14544ea40830848c12b48a57076424c8f07ae55f3cf1c1a690@ec2-174-129-18-98.compute-1.amazonaws.com:5432/ddjkl3vomv6jmf'
+    connection: 'postgres: //vybyvcqprszotj:51720ea9b7ce6e14544ea40830848c12b48a57076424c8f07ae55f3cf1c1a690@ec2-174-129-18-98.compute-1.amazonaws.com:5432/ddjkl3vomv6jmf',
     pool: {
       min: 2,
       max: 10
